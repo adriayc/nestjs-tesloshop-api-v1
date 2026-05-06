f9e2d69f7fe491755d0303ba81796f31e8e297e9
Adriano Ayala <adriano.ayc@gmail.com>
2026-05-06 17:58:58 -0400

    JwtStrategy - Parte 2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,11 +6,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+    ConfigModule,
     // TypeORM module for product entity
     TypeOrmModule.forFeature([User]),
     // Passport module (with auth strategy)
@@ -39,6 +41,6 @@ import { User } from './entities/user.entity';
       },
     }),
   ],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
