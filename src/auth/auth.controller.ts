8abded8691802fd0676c37066afca1e86523b994
Adriano Ayala <adriano.ayc@gmail.com>
2026-05-08 16:08:05 -0400

    Custom Property Decorator - GetUser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -19,7 +21,19 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard('jwt'))
-  testingPrivateRoute() {
-    return { ok: true, message: 'Private route!!!' };
+  testingPrivateRoute(
+    // @Req() request: Express.Request
+    @GetUser() user: User,
+    // @GetUser('email') user: string,
+    // @GetUser(['email', 'role', 'fullName']) user: string[],
+  ) {
+    // console.log({ user: request.user });
+    // console.log(user);
+
+    return {
+      ok: true,
+      message: 'Private route!!!',
+      user,
+    };
   }
 }
