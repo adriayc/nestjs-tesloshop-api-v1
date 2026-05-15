268a8e0d4212d531fe6df8cf2e918a1c0c0f8f88
Adriano Ayala <adriano.ayc@gmail.com>
2026-05-15 17:54:32 -0400

    Documentar DTOs

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,13 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'How many rows do you need to return?',
+  })
   @IsOptional()
   @IsNumber()
   @IsPositive()
   @Type(() => Number) // Convert to number (enableImplicitConversions)
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'How many rows do you want to skip?',
+  })
   @IsOptional()
   @IsNumber()
   @Min(0)
